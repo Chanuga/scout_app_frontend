143b575a0942e7394b31238c1d363dea4680f171
chanuga.jayathilaka <chanuga.jayathilaka@ecologital.com>
2023-11-17 11:55:33 +0530

    Logo changes

--- src/views/Hero.tsx
+++ src/views/Hero.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import { Typewriter } from "react-simple-typewriter";
+import Logo from '../assets/scl-logo.png'
 
 const navigation = [
     { name: 'Product', href: '#' },
@@ -22,9 +23,9 @@ const Hero = () => {
                         <a href="#" className="-m-1.5 p-1.5">
                             <span className="sr-only">Your Company</span>
                             <img
-                                className="h-8 w-auto"
-                                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                                alt=""
+                                className="h-16 w-auto"
+                                src={Logo}
+                                alt="scl-logo"
                             />
                         </a>
                     </div>
@@ -58,9 +59,9 @@ const Hero = () => {
                             <a href="#" className="-m-1.5 p-1.5">
                                 <span className="sr-only">Your Company</span>
                                 <img
-                                    className="h-8 w-auto"
-                                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                                    alt=""
+                                    className="h-16 w-auto"
+                                    src={Logo}
+                                    alt="scl-logo"
                                 />
                             </a>
                             <button
@@ -113,7 +114,7 @@ const Hero = () => {
                     />
                 </div>
                 <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-                    <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+                    {/* <div className="hidden sm:mb-8 sm:flex sm:justify-center">
                         <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
                             Announcing our next round of funding.{' '}
                             <a href="#" className="font-semibold text-indigo-600">
@@ -121,7 +122,7 @@ const Hero = () => {
                                 Read more <span aria-hidden="true">&rarr;</span>
                             </a>
                         </div>
-                    </div>
+                    </div> */}
                     <div className="text-center">
                         <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
                             <Typewriter
