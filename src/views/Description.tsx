345363dd9ee75971cabc295711c07a5b966f1642
chanuga.jayathilaka <chanuga.jayathilaka@ecologital.com>
2023-11-18 08:52:21 +0530

    Description changes

--- src/views/Description.tsx
+++ src/views/Description.tsx
@@ -1,30 +1,24 @@
 import { ArrowPathIcon, CloudArrowUpIcon, FingerPrintIcon, LockClosedIcon } from '@heroicons/react/24/outline'
 
 const features = [
-  {
-    name: 'Push to deploy',
-    description:
-      'Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi. Odio urna massa nunc massa.',
-    icon: CloudArrowUpIcon,
-  },
-  {
-    name: 'SSL certificates',
-    description:
-      'Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget. Sem sodales gravida quam turpis enim lacus amet.',
-    icon: LockClosedIcon,
-  },
-  {
-    name: 'Simple queues',
-    description:
-      'Quisque est vel vulputate cursus. Risus proin diam nunc commodo. Lobortis auctor congue commodo diam neque.',
-    icon: ArrowPathIcon,
-  },
-  {
-    name: 'Advanced security',
-    description:
-      'Arcu egestas dolor vel iaculis in ipsum mauris. Tincidunt mattis aliquet hac quis. Id hac maecenas ac donec pharetra eget.',
-    icon: FingerPrintIcon,
-  },
+    {
+        name: 'ආගම පිළිබඳ යුතුකම',
+        description:
+            'තම ආගමෙහි ඇති ආධ්‍යාත්මික ධර්මතාවයන්ට අනුකූලව හා එයට පක්ෂපාතීව දිවිපෙවෙත සාදා ගැනීම.',
+        icon: FingerPrintIcon,
+    },
+    {
+        name: 'අන් අය කෙරෙහි යුතුකම',
+        description:
+            'ප්‍රාදේශීය, දේශිය සහ ජාත්‍යන්තර සංහිඳියාව හරහා තම රටට පක්ෂපාතී වීම හා ජගත් සහයෝගීතාවය සහ මනුෂ්‍යත්වය මුලකොටගෙන සමාජ අභිමනය හා ගරුත්වය පදනම් කර ගත් සමාජ සංවර්ධන වැඩ සඳහා සහභාගී වීම.',
+        icon: FingerPrintIcon,
+    },
+    {
+        name: 'තමා පිළිබඳ යුතුකම',
+        description:
+            'තමාගේ අභිවෘද්ධිය සඳහා ඇති යුතුකම.',
+        icon: FingerPrintIcon,
+    },
 ]
 
 const Description = () => {
@@ -32,15 +26,38 @@ const Description = () => {
         <div className="bg-white py-24 sm:py-32">
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
                 <div className="mx-auto max-w-2xl lg:text-center">
-                    <h2 className="text-base font-semibold leading-7 text-indigo-600">Deploy faster</h2>
                     <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                        Everything you need to deploy your app
+                        බාලදක්ෂ ව්‍යාපාරය
                     </p>
                     <p className="mt-6 text-lg leading-8 text-gray-600">
-                        Quis tellus eget adipiscing convallis sit sit eget aliquet quis. Suspendisse eget egestas a elementum
-                        pulvinar et feugiat blandit at. In mi viverra elit nunc.
+                        බාලදක්ෂ ව්‍යාපාරය යනු එහි නිර්මාතෘ වරයා විසින් හඳුනාගත් අරමුණු, සිද්ධාන්තයන් සහ ක්‍රමවේදයන්ගෙන් සමන්විත කිසිඳු භේදයකින් තොරව ළමා හා තරුණයන්ගේ අධ්‍යපනය සඳහා සැකසුනු, සැමට විවෘත්ත වූ හා දේශපාලනයකින් තොර වූ ස්වෙච්ඡා සංවිධානයකි.
                     </p>
                 </div>
+
+                <div className="mx-auto max-w-2xl lg:text-center mt-16">
+                    <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                        බාලදක්ෂ මෙහෙවර ප්‍රකාශය
+                    </p>
+                    <p className="mt-6 text-lg leading-8 text-gray-600">
+                        ස්වයං පරිසමාප්ත පුද්ගලයින් වශයෙන් සමාජයට ඵලදායී කාර්යභාරයක් ඉටුකරමින් වඩාත් යහපත් ලොවක් ගොඩනැඟීම සඳහා සහය වීමට, බාලදක්ෂ නීතිය හා පොරොන්දුව මත පදනම් වූ පිළිගත් අගයන් මඟින් ළමා හා යොවුන් අධ්‍යාපනයට දායක වීම බාලදක්ෂ ව්‍යාපාරයේ මෙහෙවරයි.
+                    </p>
+                </div>
+
+                <div className="mx-auto max-w-2xl lg:text-center mt-16">
+                    <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                        බාලදක්ෂ මෙහෙවර ප්‍රකාශය
+                    </p>
+                    <p className="mt-6 text-lg leading-8 text-gray-600">
+                        බාලදක්ෂ ව්‍යාපාරයේ මූලික අරමුණු වන්නේ ළමා සහ තරුණ පරපුර ශාරීරික, බුද්ධිමය, සංවේදී, සමාජයීය, ැධ්‍යාත්මික හා සංස්කෘතික හැකියාවන් වර්ධනය කිරීම තුලින් ප්‍රාදේශීය, ජාතික සහ ජාත්‍යන්තර ප්‍රජාවට වැඩදායී වූ වගකිය හැකි යහපත් පුරවැසියෙකු බිහි කිරීමයි.
+                    </p>
+                </div>
+
+                <div className="mx-auto max-w-2xl lg:text-center mt-16">
+                    <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                    බාලදක්ෂ ව්‍යාපාරයේ මූලධර්ම
+                    </p>
+                </div>
+
                 <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
                     <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
                         {features.map((feature) => (
--- src/views/Hero.tsx
+++ src/views/Hero.tsx
@@ -4,7 +4,7 @@ import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import { Typewriter } from "react-simple-typewriter";
 import Logo from '../assets/scl-logo.png'
 import { motion } from "framer-motion";
-import { fadeIn } from '../utilities/configs/variants';
+import { scale } from '../utilities/configs/variants';
 import { transition } from '../utilities/configs/transition';
 
 const navigation = [
@@ -19,7 +19,6 @@ const Hero = () => {
 
     return (
         <div className="bg-white">
-
             <header className="absolute inset-x-0 top-0 z-50">
                 <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
                     <div className="flex lg:flex-1">
@@ -117,17 +116,8 @@ const Hero = () => {
                     />
                 </div>
                 <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-                    {/* <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-                        <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-                            Announcing our next round of funding.{' '}
-                            <a href="#" className="font-semibold text-indigo-600">
-                                <span className="absolute inset-0" aria-hidden="true" />
-                                Read more <span aria-hidden="true">&rarr;</span>
-                            </a>
-                        </div>
-                    </div> */}
                     <motion.div
-                        variants={fadeIn("down")}
+                        variants={scale()}
                         transition={transition()}
                         initial="hidden"
                         whileInView="visible"
@@ -151,28 +141,6 @@ const Hero = () => {
                             </p>
                         </div>
                     </motion.div>
-                </div>
-                {/* <div>
-                    <motion.img
-                        key={Banner}
-                        src={Banner}
-                        initial={{ opacity: 0, y: 200 }}
-                        animate={{ opacity: 1 }}
-                        exit={{ opacity: 0 }}
-                        transition={transition()}
-                    />
-                </div> */}
-                <div
-                    className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-                    aria-hidden="true"
-                >
-                    <div
-                        className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-                        style={{
-                            clipPath:
-                                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-                        }}
-                    />
                 </div>
             </div>
         </div>
